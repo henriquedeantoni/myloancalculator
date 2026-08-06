39fe52d2519dbbe5f97df3454ff501d1821703de
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-08-06 16:57:40 -0300

    feat: adapter and data class for news and news listing

---
+++ app/src/main/java/com/henrianthony/myloancalculator/NewsAdapter.kt
@@ -0,0 +1,46 @@
+package com.henrianthony.myloancalculator
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsAdapter (
+    private val newsList: List<News>,
+    private val onDoubleClick: (News) -> Unit
+) : RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private var lastClickTime = 0L
+
+        fun bind(news: News) {
+            val titulo = itemView.findViewById<TextView>(R.id.titulo)
+            val resumo = itemView.findViewById<TextView>(R.id.resumo)
+
+            titulo.text = news.title
+            resumo.text = news.review
+
+            itemView.setOnClickListener {
+                val now = System.currentTimeMillis()
+                if (now - lastClickTime < 500) {
+                    // TODO check the time to double click
+                    onDoubleClick(news)
+                }
+                lastClickTime = now
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_news, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(newsList[position])
+    }
+
+    override fun getItemCount() = newsList.size
+}
