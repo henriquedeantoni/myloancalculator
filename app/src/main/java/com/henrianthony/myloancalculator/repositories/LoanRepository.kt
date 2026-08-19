d3f28e44674b9b6c007cbd5946791435385c74ee
Henrique De Antoni <henriqdeantoni@gmail.com>
2026-08-18 23:36:13 -0300

    feat: loan repository methods

--- app/src/main/java/com/henrianthony/myloancalculator/repositories/LoanRepository.kt
+++ app/src/main/java/com/henrianthony/myloancalculator/repositories/LoanRepository.kt
@@ -1,4 +1,28 @@
 package com.henrianthony.myloancalculator.repositories
 
-class LoanRepository {
+import com.henrianthony.myloancalculator.data.LoanDao
+import com.henrianthony.myloancalculator.model.Loan
+
+class LoanRepository (
+    private val loanDao: LoanDao
+) {
+    suspend fun insertLoan(loan: Loan) {
+        loanDao.insertLoan(loan)
+    }
+
+    suspend fun searchAll(): List<Loan> {
+        return loanDao.searchAll()
+    }
+
+    suspend fun searchById(id: Long): Loan?{
+        return loanDao.searchById(id)
+    }
+
+    suspend fun updateLoan(loan: Loan){
+        loanDao.updateLoan(loan)
+    }
+
+    suspend fun deleteLoan(loan: Loan){
+        loanDao.deleteLoan(loan)
+    }
 }
